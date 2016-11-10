2992c641fb0b0b16cda25e1507734e81998de5d8
cemen <cemen@ACERSMALL>
2016-11-10 22:27:51 +0100

    Load levels implemented

--- LightsOut/LightsOutDomain/GameLogic.cs
+++ LightsOut/LightsOutDomain/GameLogic.cs
@@ -8,5 +8,17 @@ namespace LightsOutDomain
 {
     public class GameLogic
     {
+        public int[,] GameField { get; private set; }
+        public string LevelName { get; private set; }
+
+        public void LoadLevel(string levelName, int xSize, int ySize, int[] enabledLampNumbers)
+        {
+            GameField = new int[xSize,ySize];
+            foreach(int enabledLampNumber in enabledLampNumbers)
+            {
+                GameField[enabledLampNumber % xSize, enabledLampNumber / xSize] = 1;
+            }
+            LevelName = levelName;
+        }
     }
 }
---
+++ LightsOut/LightsOutDomainTests/GameLogicLoadTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using LightsOutDomain;
+
+namespace LightsOutDomainTests
+{
+    [TestFixture]
+    public class GameLogicLoadTests
+    {
+        [Test]
+        public void WhenLoadLevelShouldSetTheGameField()
+        {
+            GameLogic sut = new GameLogic();
+            sut.LoadLevel("Level1", 4, 4, new int[]{ 0, 5, 10, 15});
+            Assert.AreEqual(1, sut.GameField[0, 0]);
+            Assert.AreEqual(1, sut.GameField[1, 1]);
+            Assert.AreEqual(1, sut.GameField[2, 2]);
+            Assert.AreEqual(1, sut.GameField[3, 3]);
+            Assert.AreEqual(0, sut.GameField[0, 1]);
+        }
+        [Test]
+        public void WhenLoadLevelShouldSetLevelName()
+        {
+            GameLogic sut = new GameLogic();
+            sut.LoadLevel("Level1", 4, 4, new int[] {});
+            Assert.AreEqual("Level1", sut.LevelName);
+        }
+    }
+}
