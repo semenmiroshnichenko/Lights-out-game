0324ad3bb0d5a4cef6ebb1e976582c258b7943e3
cemen <cemen@ACERSMALL>
2016-11-11 22:33:13 +0100

    Starting with game logic

---
+++ LightsOut/LightsOutDomainTests/GameLogicStartGameTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using LightsOutDomain;
+
+namespace LightsOutDomainTests
+{
+    [TestFixture]
+    public class GameLogicStartGameTests
+    {
+        private GameLogic sut;
+        [SetUp]
+        public void Setup()
+        {
+            GameLogic sut = new GameLogic();
+            sut.LoadLevel("Level1", 4, 4, new int[] { 0, 5, 10, 15 });
+        }
+
+        [Test]
+        public void WhenClickOnBulb00ShouldTriggerOnly01And10Bulbs()
+        {
+            Assert.Fail();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Lights-out-game
 
 Installation: TBD
+
+
+Status: 
+
+[![Build Status](https://travis-ci.org/semenmiroshnichenko/Lights-out-game.svg?branch=master)](https://travis-ci.org/semenmiroshnichenko/Lights-out-game)
