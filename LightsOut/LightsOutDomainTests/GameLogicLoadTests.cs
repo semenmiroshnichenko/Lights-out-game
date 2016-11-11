e2b8fccf590f682c0a809c3e2be1786b45c90e17
cemen <cemen@ACERSMALL>
2016-11-11 21:37:06 +0100

    New game field size test

--- LightsOut/LightsOutDomainTests/GameLogicLoadTests.cs
+++ LightsOut/LightsOutDomainTests/GameLogicLoadTests.cs
@@ -12,7 +12,17 @@ namespace LightsOutDomainTests
     public class GameLogicLoadTests
     {
         [Test]
-        public void WhenLoadLevelShouldSetTheGameField()
+        public void WhenLoadLevelShouldInitGameField()
+        {
+            GameLogic sut = new GameLogic();
+            sut.LoadLevel("Level1", 4, 4, new int[] { 0, 5, 10, 15 });
+            Assert.NotNull(sut.GameField);
+            Assert.AreEqual(16, sut.GameField.Length);
+            Assert.AreEqual(4, sut.GameField.GetLength(0));
+            Assert.AreEqual(4, sut.GameField.GetLength(1));
+        }
+        [Test]
+        public void WhenLoadLevelShouldFillTheGameField()
         {
             GameLogic sut = new GameLogic();
             sut.LoadLevel("Level1", 4, 4, new int[]{ 0, 5, 10, 15});
